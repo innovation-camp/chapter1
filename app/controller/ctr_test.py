73bc875e3167155c5242e03202930ae286bddbf4
Lee Dajeong <chic519@gmail.com>
2022-08-02 17:34:34 +0800

    책등록 api 구현

--- app/controller/ctr_test.py
+++ app/controller/ctr_test.py
@@ -36,3 +36,36 @@ def TestDBConncect():
                     <h1>DB connection <b style="color:green">Succeeded! 🥳</b></h1>'
                 </div>
                 '''
+
+# 새 글 등록하기
+@bp.route('/posts', methods=["POST"])
+def board_create():
+    db = get_db()
+    title_receive = request.form['title_give']
+    description_receive = request.form['description_give']
+    is_selling_receive = request.form['is_selling_give']
+    condition_receive = request.form['condition_give']
+    price_receive = request.form['price_give']
+    location_receive = request.form['location_give']
+    genre_receive = request.form['genre_give']
+    contact_receive = request.form['contact_give']
+    count = list(db.board.find({}, {'_id': False}))
+    num = len(count) + 1
+    doc = {
+        'num': num,
+        'title': title_receive,
+        'description': description_receive,
+        'is_selling': is_selling_receive,
+        'condition' : condition_receive,
+        'price': price_receive,
+        'location' : location_receive,
+        'genre' : genre_receive,
+        'contact' : contact_receive,
+        'is_soldout': 0,
+        'writer': "",
+        'created_at': datetime.datetime.now(),
+        'updated_at': datetime.datetime.now()
+
+    }
+    db.board.insert_one(doc)
+    return jsonify({'msg':'글이 성공적으로 작성되었습니다!'})
